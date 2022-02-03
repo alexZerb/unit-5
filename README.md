9295bc41c9eae05a56bbfbd200c18dbe087222cd
Alex Szczerba <Szczerba18@icloud.com>
2022-02-03 01:05:32 -0700

    phone number change
    phone number is now formatted correctly using reg expression
    
    added comments, cleaned up code

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Unit-6
- Unit 5 project 
+ Public API Requests
 
 JavaScript Fullstack techdegree project for Team Treehouse
 
-Fetch user info API from https://randomuser.me/api . Creates Random profiles with image and information. Click on profile to show modal with more user info.
+Fetch user info API from https://randomuser.me/api . Creates Random profiles with image and information. Click on profile to show modal with more user info. Refreshing the page will get a new 12 random employees. Color changes to the body, h1 element(font sizing as well), and card element have been changed. When hovering over a card the color is white providing more contrast to the application. When a card is selected you can either click on the x button in the top right corner of the card or click away from the card. 
+
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-//this project is going to be challenging and fun!
-
-
+//
+//                  CONSTANTS & DECLARATIONS
+//
 const searchContainer = document.querySelector('.search-container');
 const gallery = document.querySelector('.gallery');
 const cards = document.querySelectorAll('.card');
@@ -9,21 +9,9 @@ const randomUsers = "https://randomuser.me/api/?nat=us&results=12";
 let userData = [];
 let cardData = [];
 let cardIndex = 0;
-
-
-
-
-const formHTML = `
-    <form action="#" method="get">
-    <input type="search" id="search-input" class="search-input" placeholder="Search...">
-    <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
-    </form>
-    `;
-
-
-
-
-//     FETCH FUNCTION
+//
+//         FETCH FUNCTION
+//
 function fetchUsers(url){
     return fetch(url)
         .then(res => res.json())
@@ -32,15 +20,12 @@ function fetchUsers(url){
             userData.push(users);
             getCards(users);
             getModals(users);
-            console.log(users)
-         })
-         
+         })         
 }
-    
-fetchUsers(randomUsers);
-
-
+    fetchUsers(randomUsers);
+//
 //     Create & append div card for each random user
+//
 function getCards(users){
     userInfo = userData[0].results;
     userInfo.map( userInfo => {
@@ -48,7 +33,7 @@ function getCards(users){
             div.innerHTML = `
                 <div class="card">
                 <div class="card-img-container">
-                    <img class="card-img" src="${userInfo.picture.thumbnail}" alt="profile picture">
+                    <img class="card-img" src="${userInfo.picture.large}" alt="profile picture">
                 </div>
                 <div class="card-info-container">
                     <h3 id="name" class="card-name cap">${userInfo.name.first} ${userInfo.name.last}</h3>
@@ -60,41 +45,41 @@ function getCards(users){
         gallery.appendChild(div);
     })
 }
-//     Generate modal function
-
-function getModals(user) {
-
+//
+//     Generate modal function (birthdate is sliced to create a more readable format, )
+//
+function getModals(users) {
     userInfo = userData[0].results;
     userInfo.map( userInfo => {
         let dob = userInfo.dob.date.slice(0, 10);
         let year = dob.slice(0, 4);
         let month = dob.slice(5, 7);
         let day = dob.slice(8, 10);
+        const newNumber = userInfo.phone.replace(/-/," ");
         
         modalHTML = `
             <div class="modal-container">
             <div class="modal">
                 <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
             <div class="modal-info-container">
-                <img class="modal-img" src="${userInfo.picture.thumbnail}" alt="profile picture">
+                <img class="modal-img" src="${userInfo.picture.large}" alt="profile picture">
                 <h3 id="name" class="modal-name cap">${userInfo.name.first} ${userInfo.name.last}</h3>
                 <p class="modal-text">${userInfo.email}</p>
-                <p class="modal-text cap">${userInfo.city}</p>
+                <p class="modal-text cap">${userInfo.location.city}</p>
                 <hr>
-                <p class="modal-text">${userInfo.phone}</p>
+                <p class="modal-text">${newNumber}</p>
                 <p class="modal-text">${userInfo.location.street.number} ${userInfo.location.street.name}</p>
                 <p class="modal-text">${userInfo.location.city}, ${userInfo.location.state}</p>
                  <p class="modal-text">Birthday: ${month}/${day}/${year}</p>
-                </div>
-             </div>
+            </div>
+            </div>
              `;
         cardData.push(modalHTML);
     })
-
 }
-
-//     Adds Event Listeners for each card (modal feature)
- 
+//
+// Adds Event Listeners to pull up a card when clicked (modal feature)
+// 
 document.addEventListener('click', e =>{
     const cards = document.querySelectorAll('.card');  
     if(e.target.closest('.card')) {
@@ -102,18 +87,16 @@ document.addEventListener('click', e =>{
             if (e.composedPath().includes(cards[i]))
             cardIndex = i;
         }
-    gallery.insertAdjacentHTML('afterbegin', cardData[cardIndex])  
-        
+    gallery.insertAdjacentHTML('afterbegin', cardData[cardIndex])      
     }
 })
-
-
-
+//
+//     Event listener removes card when clicked away or X button is clicked
+//
 document.addEventListener('click', e => {
     const closeBTN = document.querySelector('.modal-close-btn');
     const modalConainer = document.querySelector('.modal-container');
     if(e.target === closeBTN | e.target === modalConainer | e.target === closeBTN.firstChild) {
         modalConainer.style.display = 'none';
     }
-
 })
