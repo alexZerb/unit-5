b807c717dec9a166eded84118bd2d074d1021367
Alex Szczerba <Szczerba18@icloud.com>
2022-02-02 19:33:55 -0700

    getCards function
    when page loads new 12 users are added to page with required profile information

--- script.js
+++ script.js
@@ -1,34 +1,70 @@
 //this project is going to be challenging and fun!
 
-// Search markup: 
-
-// You can use the commented out markup below as a template
-// for your search feature and append it to this `search-container` div.
-
-// IMPORTANT: Altering the arrangement of the markup and the 
-// attributes used may break the styles or functionality.
 const searchContainer = document.querySelector('.search-container');
+const gallery = document.querySelector('.gallery');
+const randomUsers = "https://randomuser.me/api/?nat=us&results=12";
+let userData = [];
+let cardArray = [];
+
+
+
 const formHTML = `
     <form action="#" method="get">
     <input type="search" id="search-input" class="search-input" placeholder="Search...">
     <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
     </form>
     `;
-searchContainer.innerHTML = formHTML;
 
-const galleryContainer = document.querySelector('.gallery');
-console.log(galleryContainer);
 
-galleryHTML = `
-    <div class="card">
-    <div class="card-img-container">
-        <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
-    </div>
-    <div class="card-info-container">
-        <h3 id="name" class="card-name cap">first last</h3>
-        <p class="card-text">email</p>
-        <p class="card-text cap">city, state</p>
-    </div>
-    </div>
-    `;
+
+
+//     FETCH FUNCTION
+function fetchUsers(url){
+    return fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            const users = data;
+            userData.push(users);
+            getCards(users);
+         })
+         
+}
+    
+fetchUsers(randomUsers);
+
+
+//     Create & append div card for each random user
+function getCards(users){
+    userInfo = userData[0];
+    userInfo.results.map( userInfo => {
+        const div = document.createElement('div');
+            div.innerHTML = `
+                <div class="card">
+                <div class="card-img-container">
+                    <img class="card-img" src="${userInfo.picture.thumbnail}" alt="profile picture">
+                </div>
+                <div class="card-info-container">
+                    <h3 id="name" class="card-name cap">${userInfo.name.first} ${userInfo.name.last}</h3>
+                    <p class="card-text">${userInfo.email}</p>
+                    <p class="card-text cap">${userInfo.location.city}, ${userInfo.location.state}</p>
+                </div>
+                </div>
+                `;
+        gallery.appendChild(div)
+    })
+}
+    
+
+
+
+
+
+//     Adds Event Listeners for each card (modal feature)
+
+
+
+
+
+
+
 
