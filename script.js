ea2427ef7dd14abb72deb14a8fc5ac3896dd31ad
Alex Szczerba <Szczerba18@icloud.com>
2022-02-02 20:39:42 -0700

    getModals
    working on adding html for each modal

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 const searchContainer = document.querySelector('.search-container');
 const gallery = document.querySelector('.gallery');
+const cards = document.querySelectorAll('.card');
 const randomUsers = "https://randomuser.me/api/?nat=us&results=12";
 let userData = [];
 let cardArray = [];
@@ -26,6 +27,8 @@ function fetchUsers(url){
             const users = data;
             userData.push(users);
             getCards(users);
+            getModals(users);
+            console.log(users)
          })
          
 }
@@ -35,8 +38,8 @@ fetchUsers(randomUsers);
 
 //     Create & append div card for each random user
 function getCards(users){
-    userInfo = userData[0];
-    userInfo.results.map( userInfo => {
+    userInfo = userData[0].results;
+    userInfo.map( userInfo => {
         const div = document.createElement('div');
             div.innerHTML = `
                 <div class="card">
@@ -55,13 +58,46 @@ function getCards(users){
 }
     
 
+//     Generate modal function
 
+function getModals(user) {
 
+    userInfo = userData[0].results;
+    userInfo.map( userInfo => {
+        let dob = userInfo.dob.date.slice(0, 10);
+        let year = dob.slice(0, 4);
+        let month = dob.slice(5, 7);
+        let day = dob.slice(8, 10);
+        console.log(year);
+        console.log(month);
+        console.log(day);
 
+        
+        
+
+        
+        modalHTML = `
+            <div class="modal-container">
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+                <h3 id="name" class="modal-name cap">name</h3>
+                <p class="modal-text">${userInfo.email}</p>
+                <p class="modal-text cap">${userInfo.city}</p>
+                <hr>
+                <p class="modal-text">${userInfo.phone}</p>
+                <p class="modal-text">123 Portland Ave., Portland,  OR 97204</p>
+                 <p class="modal-text">Birthday: 10/21/2015</p>
+                </div>
+             </div>
+             `;
+    })
+
+}
 
 //     Adds Event Listeners for each card (modal feature)
-
-
+    
 
 
 
